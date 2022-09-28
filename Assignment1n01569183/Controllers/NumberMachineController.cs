0c1ea338db1bfd3916143f4e5bf1e3b6919333b1
Shavin <shavinsingh1999@gmail.com>
2022-09-28 12:29:57 -0400

    Add project files.

---
+++ Assignment1n01569183/Controllers/NumberMachineController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1n01569183.Controllers
+{
+    public class NumberMachineController : ApiController
+    {
+        /// <summary>
+        /// Takes an input ID, performs 4 mathematical operations on it, then returns the result
+        /// </summary>
+        /// <param name="id">integer input number</param>
+        /// <returns>mathematical result</returns>\
+        /// <example>
+        /// GET /api/NumberMachine/10 -> 119.3333
+        /// GET /api/NumberMachine/-5 -> 19.3333
+        /// GET /api/NumberMachine/30 -> 252.66667
+        /// </example>
+        public double Get(int id)
+        {
+            double result = (((id + 10) * 20) / 3) - 14;
+            return result;
+        }
+    }
+}
